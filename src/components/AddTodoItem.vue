0f7bebff44f521a3a374aa965ce9017393c2216f
Sean Corrales <sean.corrales@exaptive.com>
2018-05-02 13:30:00 -0500

    Todo app

---
+++ src/components/AddTodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <input
+      placeholder="Enter new todo item"
+      name="formValue"
+      type="text"
+      v-model="itemValue"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      itemValue: ''
+    };
+  },
+  name: 'AddTodoItem',
+  methods: {
+    onSubmit() {
+      this.$emit('newTodo', this.itemValue);
+      this.itemValue = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  font-size: 1rem;
+  margin: 1em 0;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span>
+      <input
+        type="checkbox"
+        value="on"
+        v-model="item.isChecked"
+      />
+    </span>
+    <span>{{ item.label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({
+        label: '',
+        isChecked: false
+      })
+    }
+  }
+};
+</script>
